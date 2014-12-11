4c807107a9c70b1b48ae9ef0d0509d3e1f856dd5
Michael Schubert <Mike@Michaels-MBP-2.hsd1.ga.comcast.net>
2014-12-11 14:19:09 -0500

    Created TableViewController and wired up to MainStoryboard

---
+++ TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Blog Reader
+//
+//  Created by Michael Schubert on 12/11/14.
+//  Copyright (c) 2014 Michael Schubert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+@property(nonatomic, strong) NSArray *titles;
+@end
