8834b93f4c9898a290fa22d4f236f4cb0967d3b3
Michael Schubert <Mike@Michaels-MBP-2.hsd1.ga.comcast.net>
2014-12-11 08:27:32 -0500

    Initial Commit

---
+++ Blog Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blog Reader
+//
+//  Created by Michael Schubert on 12/11/14.
+//  Copyright (c) 2014 Michael Schubert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blog Reader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blog Reader
+//
+//  Created by Michael Schubert on 12/11/14.
+//  Copyright (c) 2014 Michael Schubert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
