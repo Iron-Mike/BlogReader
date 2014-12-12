16f56e42527e65ad13f1a278fc7aeddbe944762d
Michael Schubert <Mike@Michaels-MBP-2.hsd1.ga.comcast.net>
2014-12-12 16:40:37 -0500

    Added posting date

--- TableViewController.m
+++ TableViewController.m
@@ -29,6 +29,7 @@
         BlogPost *blogPost = [BlogPost blogPostWithTitle:[bpDictionary objectForKey:@"title"]];
         blogPost.author = [bpDictionary objectForKey:@"author"];
         blogPost.thumbnail = [bpDictionary objectForKey:@"thumbnail"];
+        blogPost.date = [bpDictionary objectForKey:@"date"];
         [self.blogPosts addObject:blogPost];
     }
     
@@ -68,7 +69,8 @@
     }
         
     cell.textLabel.text = blogPost.title;
-    cell.detailTextLabel.text = blogPost.author;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Author: %@  Date: %@",blogPost.author, [blogPost formattedDate]];
+
     
     return cell;
 }
